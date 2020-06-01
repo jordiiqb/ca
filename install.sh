8550ca73c909361485f0b73e1f4940471d208d92
root <root@localhost.localdomain>
2020-06-01 22:14:29 +0200

    Modificació dels fitxers de configuració d'ldap per a permetre conexions segures.

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@
 rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 
-mkdir /var/lib/ldap.edt.org
+cp /opt/docker/ca.conf /etc/openldap/certs/.
+cp /opt/docker/ca.crt /etc/openldap/certs/.
+cp /opt/docker/ca.key /etc/openldap/certs/.
+cp /opt/docker/ca.srl /etc/openldap/certs/.
+cp /opt/docker/server.crt /etc/openldap/certs/.
+cp /opt/docker/server.csr /etc/openldap/certs/.
+cp /opt/docker/server.key /etc/openldap/certs/.
 
 cp /opt/docker/DB_CONFIG /var/lib/ldap
 slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d/
--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 bash /opt/docker/install.sh
-/sbin/slapd -d0
+ulimit -n 1024
+/sbin/slapd -d0 -h "ldap:/// ldaps:///" 
